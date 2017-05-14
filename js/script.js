5dfb27ad0b4712d54347c5e0904b7c8d8e5a2a23
Faunteleroy <faunteler@gmail.com>
2017-05-14 05:38:28 +0300

    На финальную проверку

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 var link = document.querySelector(".btn-form");
+var over = document.querySelector(".modal-overlay");
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  over.classList.add("modal-content-show-overlay");
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+  over.classList.remove("modal-content-show-overlay");
+});
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+    }
+    if (over.classList.contains("modal-content-show-overlay")) {
+      over.classList.remove("modal-content-show-overlay");
+    }
+  }
+});
